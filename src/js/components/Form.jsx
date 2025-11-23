71e779906845aa81f9a58e06639a5a3c8fa6a557
vanessarizop <vanessa.rizop@gmail.com>
2025-11-23 20:05:01 +0000

    se crea un todolist

--- src/js/components/Form.jsx
+++ src/js/components/Form.jsx
@@ -1,77 +1,154 @@
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-const Form=()=>{
 
-    let [task,setTask]=useState("");
-    let[list,setList]=useState([]);
-    let [hoverIndex, setHoverIndex] = useState(null);
- 
-    const handleTask= (event )=>{
-     setTask(event.target.value)
-    }
+const Form = () => {
+  let [task, setTask] = useState("");
+  let [list, setList] = useState([]);
+  let [hoverid, setHoverid] = useState(null);
+  const API_URL = "https://playground.4geeks.com/todo/";
 
-  const teclado = (event )=>{
-      if (event.key === "Enter")
-        (addTask());
-    };
+  const createUser = () => {
+    fetch(API_URL + "users/David", {
+      method: "POST",
+      body:JSON.stringify({
+        name: "David",
+        id: 24
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then((response) => {
+        if (response.status === 400) {
+          throw new Error("No se pudo crear el usuario");
+        }
+        return response.json();
+      })
+     .then((data)=>console.log(data))
+      .catch(error => console.error("Error:", error));//manejo de errores
+  };
 
+  
+////////
+
+  const obtenerTask = () => {
+    fetch(API_URL + "users/David")
+      .then((response) => {
+        if (response.status === 404) {
+          return createUser();
+          }      
+        return response.json();
+      })
+      .then(data => {
+          if (data && data.todos) {
+            setList(data.todos);
+          }})
+      .catch(error => console.error("Error:", error));//manejo de errores
+  };
+
+//////
   const addTask = (event) => {
     event.preventDefault();
-    setList([...list, task]); 
-    setTask("");//limpia
+    // setList([...list, task]);
+    //setTask("");//limpia
+    if (task.trim() === "") return;
+    fetch(API_URL + "todos/David", {
+      method: "POST",
+      body: JSON.stringify(
+        {
+          label: task,
+          is_done: false
+        }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => {
+        if (!response.ok)
+          throw new Error("No se pudo agregar la tarea");
+        return response.json();
+      })
+      .then(() => {
+        obtenerTask();//para que sincrenice la list
+        setTask("");
+      })
+      .catch(error => console.error("Error", error));
+  };
+
+  const deleteTask = (id) => {
+    fetch(API_URL + "todos/" + id, {
+      method: "DELETE"
+    })
+      .then(response => {
+        if (!response.ok) throw new Error("Error al eliminar tarea");
+        obtenerTask();//actualiza la list
+      })
+      .catch(error => console.error("Error:", error));
+  };
+
+  const deleteAllTasks = () => {
+
+    fetch(API_URL + "users/David", {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => {
+        if (!response.ok) throw new Error("Error al eliminar todas las tareas");
+        setList([]);
+        createUser();
+      })
+      .catch(error => console.error());//manejo de errores
   };
 
 
-   const deleteTask = (indexRemove) => {
-      const removeTask = list.filter((_, index) => index !== indexRemove);
-        setList(removeTask);};
-
-
-    const listTask = () => {
-      return(
-        list.map((tarea,index)=>(
-          <li key={index}
-          onMouseEnter={()=>setHoverIndex(index)}
-          onMouseLeave={()=>setHoverIndex(null)}>{tarea} 
-          {hoverIndex===index&&(<button type="button" className="close" aria-label="close"
-          onClick={()=>deleteTask(index)}>X</button> 
+  const listTask = () => {
+    return (
+      list.map((tarea) => (
+        <li key={tarea.id}
+          onMouseEnter={() => setHoverid(tarea.id)}
+          onMouseLeave={() => setHoverid(null)}>{tarea.label}
+          {hoverid === tarea.id && (<button type="button" className="close" aria-label="close"
+            onClick={() => deleteTask(tarea.id)}>X</button>
           )}
-          </li>
-        )));
-    };
+        </li>
+      )));
+  };
 
-          
+  useEffect(() => {
+    obtenerTask();
+  }, []);
 
+  const numbersTaks = (list) => {
+    let cont = list.length;
+    return `${cont} items left `
+  }
+  const teclado = (event) => {
+    if (event.key === "Enter") {
+      addTask(event);
+    }
+  };
+  const handleTask = (event) => {
+    setTask(event.target.value);
+  };
 
-const numbersTaks=(list)=>{
-  let cont=list.length;
- return `${cont} items left `
-};
-
-
-
-return(
-
-<div className="container bgimagen " style={{ width: "50rem", height: "60rem" }}>
-
-  <h1 className="d-flex justify-content-center p-4">todos</h1>
-    <div className="paper mx-5" >
-      <div className="row m-0 p-0">
- 
-        <form onSubmit ={addTask}>
-
-          <input type="text" placeholder="What you need to do " value={task} onChange={(e) => setTask(e.target.value)} />
-
-        </form>
-    <ul >{listTask(list)} </ul>
+  return (
+    <div className="container bgimagen " style={{ width: "50rem", height: "60rem" }}>
+      <h1 className="d-flex justify-content-center p-4">todos</h1>
+      <div className="paper mx-5" >
+        <div className="row m-0 p-0">
+          <form onSubmit={addTask}>
+            <label htmlFor="task" className="visually-hidden">Nueva Tarea</label>
+            <input  type="text" id="task" name="new" placeholder="What you need to do" value={task} onChange={handleTask} className="form-control" />
+          </form>
+          <ul >{listTask()} </ul>
+        </div>
+        <div className="footer ">{numbersTaks(list)} {"    "} <button type="button" className="btn btn-outline-secondary" onClick={deleteAllTasks}>Delete all tasks</button>
+        </div>
       </div>
-
-
-
-<div className="footer ">{numbersTaks(list)}</div>
- </div>
- </div>
-)};
-
+    </div>
+  );
+};
 export default Form;
